3866262d22c5b40e528ea76a1d5a1c106bbc7970
manojkannans-toast <125260971+manojkannans-toast@users.noreply.github.com>
2023-02-16 13:22:50 +0530

    Update TestEmployee.java

--- Payroll Management/Part1/TestEmployee.java
+++ Payroll Management/Part1/TestEmployee.java
@@ -24,8 +24,6 @@ public class TestEmployee{
                 String empsalary = input.nextLine();
                 employee.setSalary(empsalary);
                 employees.add(employee);
-                // input.close();
-                // option.close();
             }
             else{
                 break;
