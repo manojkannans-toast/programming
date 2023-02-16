23ccfab7b5e05d6e94c556f04ddcf4d8de0f96ee
manojkannans-toast <125260971+manojkannans-toast@users.noreply.github.com>
2023-02-16 13:14:10 +0530

    Add files via upload

--- Payroll Management/Part2/AttendanceMaster.java
+++ Payroll Management/Part2/AttendanceMaster.java
@@ -1,7 +1,39 @@
-import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Scanner;
 public class AttendanceMaster{
-    HashMap<Employee,Integer> empattendance = new HashMap<Employee,Integer>();
-    public AttendanceMaster(HashMap<Employee,Integer> empattendance){
-        this.empattendance = empattendance;
+    private LinkedHashSet<EmployeeAttendance<Employee,Integer>> empattendance;
+    public AttendanceMaster(){
+        this.empattendance = new LinkedHashSet<EmployeeAttendance<Employee,Integer>>();
+    }
+    public void setAttendance(Employee employee, String days){
+        Scanner input = new Scanner(System.in);
+        try{
+            int attendancedays = Integer.parseInt(days);
+            if(attendancedays > 0){
+                EmployeeAttendance<Employee,Integer> emp_attendance = new EmployeeAttendance<Employee,Integer>(employee,attendancedays);
+                this.empattendance.add(emp_attendance);
+            }
+            else{
+                System.out.println("WRONG ENTRY!! PLEASE ENTER THE VALID ATTENDANCE DAYS");
+                days = input.nextLine();
+                setAttendance(employee,days);
+                input.close();
+            }
+        }catch(Exception ex){
+            System.out.println("WRONG ENTRY!! PLEASE ENTER THE VALID ATTENDANCE DAYS ");
+            days = input.nextLine();
+            setAttendance(employee,days);
+            input.close();
+        }
+    }
+    public void showEligibleList(){
+        for(EmployeeAttendance<Employee,Integer> attendance : this.empattendance){
+            Integer num_of_days = attendance.getValue();
+            if(num_of_days > 10){
+                String details = attendance.getKey().toString();
+                System.out.println(details);
+            }
+        }
     }
 }
