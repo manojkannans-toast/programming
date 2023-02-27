545e5fe20aad625d15fd6222fe28ae7b04c77260
manojkannans-toast <125260971+manojkannans-toast@users.noreply.github.com>
2023-02-27 09:08:12 +0530

    Update AttendanceMaster.java

--- Payroll Management/Part2/AttendanceMaster.java
+++ Payroll Management/Part2/AttendanceMaster.java
@@ -1,22 +1,34 @@
-import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 public class AttendanceMaster{
-    private LinkedHashSet<EmployeeAttendance<Employee,Integer>> empattendance;
+    private LinkedHashMap<Employee,Integer> empattendance;
     public AttendanceMaster(){
-        this.empattendance = new LinkedHashSet<EmployeeAttendance<Employee,Integer>>();
+        this.empattendance = new LinkedHashMap<Employee,Integer>();
     }
-    public LinkedHashSet<EmployeeAttendance<Employee,Integer>> getEmployeeAttendance(){
+    public LinkedHashMap<Employee,Integer> getEmployeeAttendance(){
         return this.empattendance;
+    }
+    public int updateAttendance(int employeeid,int employeeattendance){
+        for(Map.Entry<Employee,Integer> attendance:this.empattendance.entrySet()){
+            if(attendance.getKey().getEmpId() == employeeid){
+                this.empattendance.replace(attendance.getKey(),employeeattendance);
+                return 1;
+            }
+        }
+        return 0;
     }
     public void showEligibleList(){
         if(this.empattendance.size() == 0){
             System.out.println("NO ELIGIBLE EMPLOYEES WERE THERE");
         }
-        for(EmployeeAttendance<Employee,Integer> attendance : this.empattendance){
-            Integer num_of_days = attendance.getValue();
-            if(num_of_days > 10){
-                String details = attendance.getKey().toString();
-                System.out.println(details);
+        else{
+            for(Map.Entry<Employee,Integer> attendance:this.empattendance.entrySet()){
+                Integer num_of_days = attendance.getValue();
+                if(num_of_days > 10){
+                    String details = attendance.getKey().toString();
+                    System.out.println(details);
+                }
             }
         }
     }
