e43e6f0c5b0be2d20f82cd36a5bf4ee0d1e498c3
manojkannans-toast <125260971+manojkannans-toast@users.noreply.github.com>
2023-05-09 17:49:23 +0530

    MVC project

---
+++ LoginApplication/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginApplication.Models;
+using System.Collections.Generic;
+namespace LoginApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult LoginDesign()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult validate(LoginViewModel model)
+        {
+            List<KeyValuePair<string,string>> list = new List<KeyValuePair<string,string>>();
+            list.Add(new KeyValuePair<string, string>("manoj", "manoj@01"));
+            list.Add(new KeyValuePair<string, string>("dharun", "dharun@04"));
+            list.Add(new KeyValuePair<String, string>("admin", "password"));
+            if (ModelState.IsValid)
+            {
+                if (list.Contains(new KeyValuePair<string, string>(model.UserName, model.Password)))
+                {
+                    return RedirectToAction("Dashboard", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password");
+                }
+            }
+            return View("LoginDesign", model);
+        }
+    }
+}
---
+++ LoginApplication/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LoginApplication.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
