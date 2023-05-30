bf30faa146d457043853d958305589a426be6371
manojkannans-toast <125260971+manojkannans-toast@users.noreply.github.com>
2023-05-30 19:05:28 +0530

    Delete login/src/Server directory

--- login/src/Server/api.js
+++
@@ -1,7 +0,0 @@
-const { reset } = require('nodemon');
-const dboperations = require('./index');
-//var Db=require('./index');
-
-dboperations.getLoginDetails().then(result=>{
- console.log(result);
-})
--- login/src/Server/dbconfig.js
+++
@@ -1,2 +0,0 @@
-const connectionString="server=PF3ZT1N4\\SQLEXPRESS;Database=Logindb;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-module.exports=connectionString;
